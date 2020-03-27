56a8d5f15a1a7d7023cbf074496574f31892144b
Marius Gebhard <M.Gebhard@trio-group.de>
2020-03-27 17:50:37 +0100

    Added info to footer.

--- src/components/CustomFooter.vue
+++ src/components/CustomFooter.vue
@@ -1,9 +1,15 @@
 <template>
     <footer class="custom-footer">
         <div class="custom-footer__content">
-            <span class="custom-footer__copyright">
-                &copy 2020 - Marius Gebhard
-            </span>
+            <div class="custom-footer__copyright">
+                <span class="custom-footer__copyright-heart">
+                    Created with
+                    <font-awesome-icon icon="heart"></font-awesome-icon>
+                    and
+                    <font-awesome-icon icon="coffee"></font-awesome-icon>.
+                </span>
+                <span>&copy 2020 - Marius Gebhard</span>
+            </div>
             <ul class="custom-footer__anchor-list">
                 <li class="custom-footer__anchor-list-item">
                     <a href="#">
