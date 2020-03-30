2bfc45389bdebb5eaa4da374890b967ca939df80
Marius Gebhard <M.Gebhard@trio-group.de>
2020-03-30 20:02:59 +0200

    Changed technology headlines to h3s.

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -33,7 +33,7 @@
                 </ul>
             </li>
             <li class="profile__item profile__item--skill">
-                FE Experience
+                <h3>FE Experience</h3>
                 <div class="profile__item-description">
                     I have 4 years of frontend experience with the following technologies:<br><br>
                     <a  class="profile__item-description--link profile__item-description--link-with-icon"
@@ -57,7 +57,7 @@
                 </div>
             </li>
             <li class="profile__item profile__item--skill">
-                BE Experience
+                <h3>BE Experience</h3>
                 <div class="profile__item-description">
                     I have 3 1/2 years of backend experience with the following technologies:<br><br>
                     <a  class="profile__item-description--link profile__item-description--link-with-icon"
@@ -81,7 +81,7 @@
                 </div>
             </li>
             <li class="profile__item profile__item--skill">
-                Hardware
+                <h3>Hardware</h3>
                 <p class="profile__item-description">
                     In 11th class I learned on how to program micro-controllers and how they work, until today I love
                     building little things with uC's and other hardware. A good example is my project
