b25a7a724c77abbe596dbea07b57fa06f33a0052
Marius Gebhard <M.Gebhard@trio-group.de>
2020-04-22 20:06:41 +0200

    Added static colors to skill chart items.

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -36,27 +36,27 @@
                 <h3>FE Experience</h3>
                 <div class="profile__item-description">
                     I have 4 years of frontend experience with the following technologies:<br><br>
-                    <a  class="profile__item-description--link profile__item-description--link-with-icon"
-                        href="https://en.wikipedia.org/wiki/HTML5"
-                        target="_blank">
+                    <a class="profile__item-description--link profile__item-description--link-with-icon"
+                       href="https://en.wikipedia.org/wiki/HTML5"
+                       target="_blank">
                         HTML5
                         <font-awesome-icon :icon="['fab', 'html5']"></font-awesome-icon>
                     </a>
-                    <a  class="profile__item-description--link profile__item-description--link-with-icon"
+                    <a class="profile__item-description--link profile__item-description--link-with-icon"
                        href="https://vuejs.org/"
                        target="_blank">
                         Vue.js
                         <font-awesome-icon :icon="['fab', 'vuejs']"></font-awesome-icon>
                     </a>
-                    <a  class="profile__item-description--link profile__item-description--link-with-icon"
-                        href="https://sass-lang.com/"
-                        target="_blank">
+                    <a class="profile__item-description--link profile__item-description--link-with-icon"
+                       href="https://sass-lang.com/"
+                       target="_blank">
                         SASS
                         <font-awesome-icon :icon="['fab', 'sass']"></font-awesome-icon>
                     </a>
-                    <a  class="profile__item-description--link profile__item-description--link-with-icon"
-                        href="https://getbootstrap.com/"
-                        target="_blank">
+                    <a class="profile__item-description--link profile__item-description--link-with-icon"
+                       href="https://getbootstrap.com/"
+                       target="_blank">
                         Bootstrap
                         <font-awesome-icon :icon="['fab', 'bootstrap']"></font-awesome-icon>
                     </a>
@@ -66,21 +66,21 @@
                 <h3>BE Experience</h3>
                 <div class="profile__item-description">
                     I have 3 years of backend experience with the following technologies:<br><br>
-                    <a  class="profile__item-description--link profile__item-description--link-with-icon"
-                        href="https://nodejs.org/en/"
-                        target="_blank">
+                    <a class="profile__item-description--link profile__item-description--link-with-icon"
+                       href="https://nodejs.org/en/"
+                       target="_blank">
                         node.js
                         <font-awesome-icon :icon="['fab', 'node-js']"></font-awesome-icon>
                     </a>
-                    <a  class="profile__item-description--link profile__item-description--link-with-icon"
-                        href="https://en.wikipedia.org/wiki/PHP"
-                        target="_blank">
+                    <a class="profile__item-description--link profile__item-description--link-with-icon"
+                       href="https://en.wikipedia.org/wiki/PHP"
+                       target="_blank">
                         PHP7
                         <font-awesome-icon :icon="['fab', 'php']"></font-awesome-icon>
                     </a>
-                    <a  class="profile__item-description--link profile__item-description--link-with-icon"
-                        href="https://laravel.com/"
-                        target="_blank">
+                    <a class="profile__item-description--link profile__item-description--link-with-icon"
+                       href="https://laravel.com/"
+                       target="_blank">
                         Laravel
                         <font-awesome-icon :icon="['fab', 'laravel']"></font-awesome-icon>
                     </a>
@@ -92,7 +92,8 @@
                 <p class="profile__item-description">
                     In 11th class I learned on how to program micro-controllers and how they work, until today I love
                     building little things with uC's and other hardware. A good example is my project
-                    <span class="profile__item-description--link" @click="$parent.$parent.$scrollTo(2)">X-Lights</span>.
+                    <span class="profile__item-description--link" @click="$parent.$parent.$scrollTo(2)">X-Lights</span>
+                    .
                 </p>
             </li>
         </ul>
@@ -112,10 +113,9 @@
         methods:  {
             chartItemStyle(item) {
                 return {
-                    height:     `${item.percentage}%`,
-                    background: item.color,
+                    height: `${item.percentage}%`,
                 };
-            }
+            },
         },
     };
 </script>
