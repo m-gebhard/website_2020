28ce0aa778e6064232a29d98069c2ebde8bf3bfd
Marius Gebhard <M.Gebhard@trio-group.de>
2020-05-01 13:06:55 +0200

    Added background to hero area. Added font theme color for hero area.

--- src/components/HeroArea.vue
+++ src/components/HeroArea.vue
@@ -1,27 +1,30 @@
 <template>
     <section class="page-section hero-area">
-        <font-awesome-icon class="hero-area__icon"
-                           icon="code">
-        </font-awesome-icon>
-        <div class="hero-area__divider"></div>
-        <h1 class="hero-area__title">
-            marius gebhard
-        </h1>
-        <ul class="hero-area__anchor-list">
-            <li class="hero-area__anchor-list-item">
-                Frontend
-            </li>
-            <li class="hero-area__anchor-list-item">
-                Apps
-            </li>
-            <li class="hero-area__anchor-list-item">
-                Backend
-            </li>
-        </ul>
-        <a @click="$parent.$scrollTo(1)" class="hero-area__jump-caret">
-            <font-awesome-icon icon="caret-down"></font-awesome-icon>
-        </a>
-        <slot></slot>
+        <div class="hero-area-overlay"></div>
+        <div class="hero-area-content">
+            <font-awesome-icon class="hero-area__icon"
+                               icon="code">
+            </font-awesome-icon>
+            <div class="hero-area__divider"></div>
+            <h1 class="hero-area__title">
+                marius gebhard
+            </h1>
+            <ul class="hero-area__anchor-list">
+                <li class="hero-area__anchor-list-item">
+                    Frontend
+                </li>
+                <li class="hero-area__anchor-list-item">
+                    Apps
+                </li>
+                <li class="hero-area__anchor-list-item">
+                    Backend
+                </li>
+            </ul>
+            <a @click="$parent.$scrollTo(1)" class="hero-area__jump-caret">
+                <font-awesome-icon icon="caret-down"></font-awesome-icon>
+            </a>
+            <slot></slot>
+        </div>
     </section>
 </template>
 
